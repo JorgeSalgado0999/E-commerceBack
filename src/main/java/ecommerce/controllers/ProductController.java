04349d3fa48b1c7d276f8def8c9d2e72e4e51a23
Jorge Luis Salgado Ledezma <jorgesalgado_isc@outlook.com>
2023-06-02 18:16:01 -0600

    template

---
+++ src/main/java/ecommerce/controllers/ProductController.java
@@ -0,0 +1,50 @@
+package ecommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ecommerce.bl.CartMethods;
+import ecommerce.domain.Cart;
+import ecommerce.domain.Product;
+import ecommerce.domain.User;
+
+@RestController
+public class ProductController {
+
+  @Autowired
+  private CartMethods service;
+
+
+  @PostMapping("/api/product")
+  public Product createProduct(@RequestBody Product product) {
+    System.out.println("lo que llega primero: " + product);
+
+    return service.createProduct(product);
+  }
+
+  @GetMapping("/api/product")
+  public Iterable<Product> getAllProducts() {
+    return service.getAllProducts();
+  }
+
+
+  
+  @PostMapping("/api/user")
+  public User createUser(@RequestBody User user) {
+    System.out.println("lo que llega primero: " + user.getName());
+
+    return service.createUser(user);
+  }
+  
+
+  @PostMapping("/api/cart")
+  public Cart createCart(@RequestBody Cart cart) {
+    System.out.println("lo que llega primero: " + cart.toString());
+
+    return service.createCart(cart);
+  }
+
+}
---
+++ src/main/java/ecommerce/domain/User.java
@@ -0,0 +1,5 @@
+package ecommerce.domain;
+
+public class User {
+  
+}
